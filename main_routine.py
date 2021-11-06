4a84c77909fa1ce84573d57027f3eb39ea17374a
Awwal Mohammed <mohammedawwal93@gmail.com>
2021-11-07 01:43:36 +0400

    Modified git configurations.

--- main_routine.py
+++ main_routine.py
@@ -6,5 +6,4 @@ program title: main_routine.py
 description:
 This program is expected to carry out an operation such that given a number of pennies\
 will calculate the minimum number of Sterling coins equivalent to that amount.
-
 """
