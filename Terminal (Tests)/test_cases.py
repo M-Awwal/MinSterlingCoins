6eac4d81efc35d1aaa9fab0e78859abd4eb1634f
Awwal Mohammed <mohammedawwal93@gmail.com>
2021-11-09 23:54:28 +0400

    Finalized test modules

--- Terminal (Tests)/test_cases.py
+++ Terminal (Tests)/test_cases.py
@@ -7,7 +7,8 @@ description:
 \t This program performs individual explicit tests based on the provided test data (refer to README.md).
 \t We call the core functionalities of the main routine in an encapsulated manner, via test_skeleton.py.
 \t We perform all test calls free of `strong` exception-handling,
-\t we utilize the said error catching mechanism here for purely debugging purposes.
+\t we utilize the said error catching mechanism here for purely debugging purposes.\n
+\t Once you understand the program structure, feel free to add your own tests.
 
 """
 import logging
@@ -49,8 +50,93 @@ except Exception as e:
     logging.debug(e)
     logging.info(e)
 
+try:  # Test Case 6, Input: £1.33
+    test_skeleton.main('£1.33')
+    print(f"Test Case 6 Passed, Input: {'£1.33'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
 
+try:  # Test Case 7, Input: £2
+    test_skeleton.main('£2')
+    print(f"Test Case 7 Passed, Input: {'£2'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
 
+try:  # Test Case 8, Input: £20
+    test_skeleton.main('£20')
+    print(f"Test Case 8 Passed, Input: {'£20'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
 
+try:  # Test Case 9, Input: £1.97p
+    test_skeleton.main('£1.97p')
+    print(f"Test Case 9 Passed, Input: {'£1.97p'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
 
+try:  # Test Case 10, Input: £1p
+    test_skeleton.main('£1p')
+    print(f"Test Case 10 Passed, Input: {'£1p'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
 
+try:  # Test Case 11, Input: £1.p
+    test_skeleton.main('£1.p')
+    print(f"Test Case 11 Passed, Input: {'£1.p'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
+
+try:  # Test Case 12, Input: 001.61p
+    test_skeleton.main('001.61p')
+    print(f"Test Case 12 Passed, Input: {'001.61.p'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
+
+try:  # Test Case 13, Input: 6.235p
+    test_skeleton.main('6.235p')
+    print(f"Test Case 13 Passed, Input: {'6.235.p'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
+
+try:  # Test Case 14, Input: £1.256532677p
+    test_skeleton.main('£1.256532677p')
+    print(f"Test Case 14 Passed, Input: {'£1.256532677p'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
+
+try:  # Test Case 15, Input:
+    test_skeleton.main('')
+    print(f"Test Case 15 Passed, Input: {''}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
+
+try:  # Test Case 16, Input: 1x
+    test_skeleton.main('1x')
+    print(f"Test Case 16 Passed, Input: {'1x'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
+
+try:  # Test Case 17, Input: £1x.0p
+    test_skeleton.main('£1x.0p')
+    print(f"Test Case 17 Passed, Input: {'£1x.0p'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
+
+try:  # Test Case 18, Input: £p
+    test_skeleton.main('£p')
+    print(f"Test Case 18 Passed, Input: {'£p'}")
+except Exception as e:
+    logging.debug(e)
+    logging.info(e)
